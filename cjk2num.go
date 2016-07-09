e26833dd47d03422d3ab0d9bcc7e8e28e94816de
kurehajime <x.iidec+gh@googlemail.com>
2016-07-09 23:27:55 +0900

    clean

--- cjk2num.go
+++ cjk2num.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-//Symbol 数字や単位など有効なすべての文字
+//Symbol 数字や単位など有効なすべての文字------------------
 type Symbol interface {
 	//Key 対象となる文字
 	Key() string
@@ -14,14 +14,16 @@ type Symbol interface {
 	Calc(stage1, stage2, stage3 int64) (int64, int64, int64, error)
 }
 
-//BreakSymbol ex:兆億万...
+//BreakSymbol ex:兆億万...------------------
 type BreakSymbol struct {
 	key   string
 	value int64
 }
 
+//Key :get key
 func (sym BreakSymbol) Key() string { return sym.key }
 
+//Calc 直左のBreakSymbolから現在位置までの数字に対し係数をかける
 func (sym BreakSymbol) Calc(stage1, stage2, stage3 int64) (int64, int64, int64, error) {
 	if (stage2 + stage1) == 0 {
 		return 0, 0, 0, fmt.Errorf("Error:Invalid value.[ ???%s]", sym.key)
@@ -29,14 +31,16 @@ func (sym BreakSymbol) Calc(stage1, stage2, stage3 int64) (int64, int64, int64, 
 	return 0, 0, stage3 + (stage1+stage2)*sym.value, nil
 }
 
-//NonBreakSymbol ex:千百十...
+//NonBreakSymbol ex:千百十...------------------
 type NonBreakSymbol struct {
 	key   string
 	value int64
 }
 
+//Key :get key
 func (sym NonBreakSymbol) Key() string { return sym.key }
 
+//Calc 直左のBreakSymbolから現在位置までの数字に対し係数をかける
 func (sym NonBreakSymbol) Calc(stage1, stage2, stage3 int64) (int64, int64, int64, error) {
 	if stage1 == 0 {
 		return 0, stage2 + 1*sym.value, stage3, nil
@@ -44,31 +48,35 @@ func (sym NonBreakSymbol) Calc(stage1, stage2, stage3 int64) (int64, int64, int6
 	return 0, stage2 + (stage1 * sym.value), stage3, nil
 }
 
-//AllBreakSymbol ex:ダース...
+//AllBreakSymbol ex:ダース...------------------
 type AllBreakSymbol struct {
 	key   string
 	value int64
 }
 
+//Key :get key
 func (sym AllBreakSymbol) Key() string { return sym.key }
 
+//Calc 左端から現在位置までの数字に対し係数をかける
 func (sym AllBreakSymbol) Calc(stage1, stage2, stage3 int64) (int64, int64, int64, error) {
 	return 0, 0, (stage3 + stage2 + stage1) * sym.value, nil
 }
 
-//NumberSymbol ex:一二三壱弐参...
+//NumberSymbol ex:一二三壱弐参...------------------
 type NumberSymbol struct {
 	key   string
 	value int64
 }
 
+//Key :get key
 func (sym NumberSymbol) Key() string { return sym.key }
 
+//Calc 直左のNumberSymbolを10倍し係数を足す
 func (sym NumberSymbol) Calc(stage1, stage2, stage3 int64) (int64, int64, int64, error) {
 	return (stage1 * 10) + sym.value, stage2, stage3, nil
 }
 
-// Convert /漢数字|中文数字|한자 숫자/  to number
+// Convert /漢数字|中文数字|한자 숫자/  to number------------------
 func Convert(word string) (result int64, err error) {
 	runes := []rune(word)
 	var stage1, stage2, stage3 int64
